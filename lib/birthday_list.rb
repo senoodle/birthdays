b5a772d1781392f7a357167aa56ce4617f0c1210
Sibel Eren <edaeren575@gmail.com>
2021-07-05 14:30:13 +0100

    first commit

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,22 @@
 
+class Birthdays
+
+
+
+  def initialize 
+    @birthdays = []
+  end
+
+
+  def store(name, dob)
+    @birthdays << { name: name, dob: dob}
+  end
+
+  def display_list
+    @birthdays.each do |birthday|
+      puts "#{birthday[:name]}: #{birthday[:dob]}"
+    end
+  end
+
+
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,22 @@
 
+require 'birthday_list'
+
+describe Birthdays do
+
+  subject(:birthday) {described_class.new}
+
+  it 'can create an instance of a birthday' do
+    expect(birthday).to be_kind_of(Birthdays)
+  end
+
+  it 'can store birthdays' do
+    expect(birthday.store('Kinnie', '6 November 1996')).to eq [{name: 'Kinnie', dob: '6 November 1996'}]
+  end
+
+  it 'displays list of birthdays' do
+    birthday.store('Kinnie', '6 November 1996')
+    birthday.store('Melon', '25 January 1996')
+    
+    expect{ birthday.display_list }.to output('Kinnie: 6 November 1996\nMelon: 25 January 1996\n').to_stdout
+  end
+end
